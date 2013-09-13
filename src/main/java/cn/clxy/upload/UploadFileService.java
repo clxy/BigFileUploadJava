46a1b6c459eee6d24dde8f7009f66af32a600e9b
yi.cui.clxy <j_clxy@hotmail.com>
2013-09-13 15:40:01 +0800

    Put every task back ... back again ...

--- src/main/java/cn/clxy/upload/UploadFileService.java
+++ src/main/java/cn/clxy/upload/UploadFileService.java
@@ -6,11 +6,15 @@
 package cn.clxy.upload;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
@@ -24,6 +28,9 @@ import org.apache.commons.logging.LogFactory;
 public class UploadFileService {
 
 	private File file;
+	private BlockingQueue<Part> parts;
+	private List<Integer> indexes;
+
 	private Uploader uploader = new ApacheHCUploader();
 	private ExecutorService executor = Executors.newFixedThreadPool(Config.maxUpload);
 
@@ -38,20 +45,20 @@ public class UploadFileService {
 
 	public void upload() {
 		try {
-			doUpload(null);
+			doUpload();
 		} finally {
 			stop();
 		}
 	}
 
-	public void retry(Integer... indexes) {
+	public void retry(Integer... array) {
 
 		// sort first.
-		List<Integer> list = Arrays.asList(indexes);
-		Collections.sort(list);
+		indexes = Arrays.asList(array);
+		Collections.sort(indexes);
 
 		try {
-			doUpload(list);
+			doUpload();
 		} finally {
 			stop();
 		}
@@ -63,20 +70,20 @@ public class UploadFileService {
 		}
 	}
 
-	private void doUpload(final List<Integer> indexes) {
+	private void doUpload() {
 
 		log.debug("Start! ===--------------------");
 
-		BlockingQueue<Part> parts = new ArrayBlockingQueue<Part>(Config.maxRead);
+		parts = new ArrayBlockingQueue<Part>(Config.maxRead);
 		CompletionService<String> cs = new ExecutorCompletionService<String>(executor);
 
 		log.debug("Reading started.");
-		cs.submit(new ReadTask(file, indexes, parts));
+		cs.submit(readTask);
 
 		log.debug("Uploading started.");
 
 		for (int i = 0; i < Config.maxUpload; i++) {
-			cs.submit(new UploadTask("upload." + i, uploader, parts));
+			cs.submit(new UploadTask("upload." + i));
 		}
 
 		// Wait all done. total count = maxUpload + 1.
@@ -91,7 +98,7 @@ public class UploadFileService {
 		}
 
 		// Notify sever all done.
-		Future<String> result = executor.submit(new NotifyTask(file, uploader));
+		Future<String> result = executor.submit(notifyTask);
 		checkFuture(result);
 
 		log.debug("End! ===--------------------");
@@ -110,4 +117,115 @@ public class UploadFileService {
 			throw new RuntimeException(e.getCause());
 		}
 	}
+
+	private Callable<String> readTask = new Callable<String>() {
+
+		@Override
+		public String call() throws Exception {
+
+			FileInputStream fis = null;
+			String fileName = file.getName();
+			int partSize = Config.partSize;
+
+			try {
+				fis = new FileInputStream(file);
+				FileChannel fc = fis.getChannel();
+				for (int i = 0;; i++) {
+					ReadStatus status = getReadStatus(i, indexes);
+					if (status == ReadStatus.stop) {
+						break;
+					}
+
+					if (status == ReadStatus.skip) {
+						fc.position(fc.position() + partSize);
+						continue;
+					}
+
+					if (status == ReadStatus.read) {
+						ByteBuffer bb = ByteBuffer.allocate(partSize);
+						int bytesRead = fc.read(bb);
+						if (bytesRead == -1) {
+							break;
+						}
+						byte[] bytes = bb.array();
+						if (bytesRead != partSize) {// trim
+							bytes = Arrays.copyOf(bytes, bytesRead);
+						}
+						parts.put(new Part(createFileName(fileName, i), bytes));
+					}
+				}
+			} finally {
+				if (fis != null) {
+					try {
+						fis.close();
+					} catch (Exception e) {
+					}
+				}
+				parts.put(Part.NULL);// put end signal.
+			}
+
+			return "read";
+		}
+
+		/**
+		 * Create file name of part. <br>
+		 * bigfile.avi = [bigfile.avi<strong>.0</strong>, bigfile.avi.1, bigfile.avi.2 ...]
+		 * @param fileName
+		 * @param i
+		 * @return
+		 */
+		protected String createFileName(String fileName, int i) {
+			return fileName + "." + i;// start by 0.
+			// return fileName + (i == 0 ? "" : ("." + i));
+		}
+
+		private ReadStatus getReadStatus(int i, List<Integer> indexes) {
+
+			if (indexes == null || indexes.contains(i)) {
+				return ReadStatus.read;
+			}
+
+			if (i > indexes.get(indexes.size() - 1)) {
+				return ReadStatus.stop;
+			}
+
+			return ReadStatus.skip;
+		}
+	};
+
+	private static enum ReadStatus {
+		stop, skip, read
+	}
+
+	private class UploadTask implements Callable<String> {
+
+		private String name;
+
+		public UploadTask(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String call() throws Exception {
+			while (true) {
+				Part part = parts.take();
+				if (part == Part.NULL) {
+					parts.add(Part.NULL);// notify others to stop.
+					break;
+				}
+				uploader.upload(part);
+			}
+			return name;
+		}
+	}
+
+	private Callable<String> notifyTask = new Callable<String>() {
+		@Override
+		public String call() throws Exception {
+			long length = file.length();
+			long partCount = (length / Config.partSize) + (length % Config.partSize == 0 ? 0 : 1);
+			uploader.done(file.getName(), partCount);
+			return "notify";
+		}
+	};
 }
